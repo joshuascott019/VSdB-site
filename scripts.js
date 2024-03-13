7858eb003db909ed9ac86944c56109ad0845fba3
Joshua Scott <joshua.ds.s019@gmail.com>
2024-03-13 10:40:50 -0700

    feat: nav buttons now function in navigating to correct page/elements

--- scripts.js
+++ scripts.js
@@ -1,12 +1,62 @@
-const flipCard = document.querySelectorAll('.flip')
-const cardBack = document.querySelectorAll('.feat-card-back')
-const cardFront = document.querySelectorAll('.feat-card-front')
+const navItems = document.querySelectorAll(".nav-item");
+const heroEl = document.getElementById('hero')
+const novoEl = document.getElementById("novo");
+const inicioEl = document.getElementById("inicio");
+const contatoEl = document.getElementById('contato')
+const sobreEl = document.getElementById("sobre");
+const faqsEl = document.getElementById("faqs");
+const flipCard = document.querySelectorAll(".flip");
+const cardBack = document.querySelectorAll(".feat-card-back");
+const cardFront = document.querySelectorAll(".feat-card-front");
 
-flipCard.forEach((card)=>{
-    card.addEventListener('mouseenter', ()=>{
-        card.firstElementChild.style.display = 'flex'
-    })
-    card.addEventListener('mouseleave', ()=>{
-        card.firstElementChild.style.display = 'none'
-    })
-})
+const pages = [heroEl, novoEl, inicioEl, contatoEl, sobreEl, faqsEl];
+
+flipCard.forEach((card) => {
+  card.addEventListener("mouseenter", () => {
+    card.firstElementChild.style.display = "flex";
+  });
+  card.addEventListener("mouseleave", () => {
+    card.firstElementChild.style.display = "none";
+  });
+});
+
+navItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target.innerText);
+    switch (e.target.innerText) {
+      case "novo":
+        pages.forEach((page) => page.classList.add("hidden"));
+        navItems.forEach((item) => item.classList.remove("active"));
+        novoEl.classList.remove("hidden");
+        e.target.classList.add("active");
+        break;
+      case "início":
+        pages.forEach((page) => page.classList.add("hidden"));
+        navItems.forEach((item) => item.classList.remove("active"));
+        heroEl.classList.remove("hidden");
+        inicioEl.classList.remove("hidden");
+        e.target.classList.add("active");
+        break;
+        case "contato":
+          pages.forEach((page)=> page.classList.add('hidden'))
+          navItems.forEach((item) => item.classList.remove("active"));
+          contatoEl.classList.remove('hidden')
+          e.target.classList.add("active");
+          break;
+      case "sobre":
+        pages.forEach((page) => page.classList.add("hidden"));
+        navItems.forEach((item) => item.classList.remove("active"));
+        sobreEl.classList.remove("hidden");
+        e.target.classList.add("active");
+        break;
+      case "FAQ":
+        pages.forEach((page) => page.classList.add("hidden"));
+        navItems.forEach((item) => item.classList.remove("active"));
+        faqsEl.classList.remove("hidden");
+        e.target.classList.add("active");
+        break;
+      default:
+        console.log("ERROR");
+    }
+  });
+});
